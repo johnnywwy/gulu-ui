8f4f6d1401fcee245e7d0bcf3ae1a17bc94f320f
johnnywwy <johnnywwy@gmail.com>
2022-03-30 18:28:40 +0800

    完成 HTML 结构初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    Home 页面
+    <div class="topnav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>咕噜 UI</h1>
+      <h2>一个还不错的 UI 框架</h2>
+      <p>
+        <a>Github</a>
+        <a>开始</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.topnav {
+  border: 1px solid red;
+}
+</style>
