cfecba1ef2fd60b3e4863ef5deb8d5f6bdb85016
johnnywwy <johnnywwy@gmail.com>
2022-04-01 22:28:40 +0800

    添加 theme 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -3,10 +3,10 @@
     <div>Button 示例</div>
     <h1>示例1</h1>
     <div>
-      <Button @click="onClick"
-              @focus="onClick"
-              @mouseover="onClick"
-      >你好哦</Button>
+      <Button >你好哦</Button>
+      <Button theme="button">你好哦</Button>
+      <Button theme="link">你好哦</Button>
+      <Button theme="text">你好哦</Button>
     </div>
   </div>
 </template>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,12 +1,19 @@
 <template>
-  <button>
+  <button class="gulu-button"
+          :class="`theme-${theme}`"
+  >
     <slot/>
   </button>
 </template>
 
 <script lang="ts">
 export default {
-
+  props: {
+    theme: {
+      type: String,
+      default: 'button'
+    }
+  }
 };
 </script>
 
