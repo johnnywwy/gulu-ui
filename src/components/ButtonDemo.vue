dfad5ae0e6cfd8cec5b655118a2a82f6a2ae8e9b
johnnywwy <johnnywwy@gmail.com>
2022-04-02 00:18:57 +0800

    支持 level

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -3,11 +3,12 @@
     <div>Button 示例</div>
     <h1>示例1</h1>
     <div>
-      <Button >你好</Button>
+      <Button>你好</Button>
       <Button theme="button">你好</Button>
       <Button theme="link">你好</Button>
       <Button theme="text">你好</Button>
     </div>
+    <br>
     <h1>示例2</h1>
     <div>
       <Button size="big">大大大</Button>
@@ -23,6 +24,23 @@
       <Button size="big" theme="text">大大大</Button>
       <Button theme="text">普普通</Button>
       <Button size="small" theme="text">小小小</Button>
+    </div>
+    <br>
+    <h1>示例3</h1>
+    <div>
+      <Button level="main">主要按钮</Button>
+      <Button>普通按钮</Button>
+      <Button level="danger">危险按钮</Button>
+    </div>
+    <div>
+      <Button theme="link" level="main">主要链接按钮</Button>
+      <Button theme="link">普通链接按钮</Button>
+      <Button theme="link" level="danger">危险链接按钮</Button>
+    </div>
+    <div>
+      <Button theme="text" level="main">主要文字按钮</Button>
+      <Button theme="text">普通文字按钮</Button>
+      <Button theme="text" level="danger">危险文字按钮</Button>
     </div>
   </div>
 </template>
