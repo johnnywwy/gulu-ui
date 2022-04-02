99218ba24ac76e929eff9a1e8b8429df2531a9cf
johnnywwy <johnnywwy@gmail.com>
2022-04-02 17:10:27 +0800

    创建 Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <h1>Dialog 示例</h1>
+    <h1>示例1</h1>
+    <Dialog></Dialog>
   </div>
 </template>
 
 <script lang="ts">
+import Dialog from '../lib/Dialog.vue';
 
+export default {
+  components: {Dialog}
+};
 </script>
 
 <style lang="scss" scoped>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="gulu-dialog-overlay"></div>
+  <div class="gulu-dialog-wrapper">
+    <header>标题</header>
+    <main>
+      <p>第一行</p>
+      <p>第二行</p>
+    </main>
+    <footer>
+      <Button>确认</Button>
+      <Button>取消</Button>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from './Button.vue';
+
+export default {
+  components: {Button}
+
+};
+</script>
+
+<style lang="scss">
+
+</style>
