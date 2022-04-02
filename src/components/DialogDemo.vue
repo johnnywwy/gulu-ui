eaf734a2fe1dda8c7fba52c441bebac02e28bb3f
johnnywwy <johnnywwy@gmail.com>
2022-04-02 22:02:19 +0800

    完成 toggle 按钮

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -2,15 +2,25 @@
   <div>
     <h1>Dialog 示例</h1>
     <h1>示例1</h1>
-    <Dialog></Dialog>
+    <Button  @click="toggle">toggle</Button>
+    <Dialog :visible="x"></Dialog>
   </div>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue';
+import Button from '../lib/Button.vue';
+import {ref} from 'vue';
 
 export default {
-  components: {Dialog}
+  components: {Dialog, Button},
+  setup() {
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+    return {x, toggle};
+  }
 };
 </script>
 
