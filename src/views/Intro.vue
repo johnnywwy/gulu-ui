1c88664668ffb91bd1b8255e408963f2f06c4afe
johnnywwy <johnnywwy@gmail.com>
2022-04-06 16:34:37 +0800

    创建路由

---
+++ src/views/Intro.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    Intro 页面
+  </div>
+</template>
+<script lang="ts">
+export default {};
+</script>
---
+++ src/views/Start.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    开始使用
+  </div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
