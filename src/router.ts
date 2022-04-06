47d9a8cbac39f920639c57792359220a0144102a
johnnywwy <johnnywwy@gmail.com>
2022-04-06 19:18:35 +0800

    事不过三 优化markdown

--- src/router.ts
+++ src/router.ts
@@ -6,11 +6,10 @@ import ButtonDemo from './components/ButtonDemo.vue';
 import DialogDemo from './components/DialogDemo.vue';
 import TabsDemo from './components/TabsDemo.vue';
 import DocDemo from './components/DocDemo.vue';
+import Markdown from './components/Markdown.vue';
+import {h} from 'vue';
 
-import Intro from './views/Intro.vue';
-import GetStarted from './views/Start.vue';
-import Install from './views/Install.vue';
-
+const md = filename => h(Markdown, {path: `../markdown/${filename}.md`, key: filename});
 const history = createWebHashHistory();
 export const router = createRouter(
   {
@@ -20,9 +19,9 @@ export const router = createRouter(
       {
         path: '/doc', component: Doc, children: [
           {path: '', component: DocDemo},
-          {path: 'intro', component: Intro},
-          {path: 'get-started', component: GetStarted},
-          {path: 'install', component: Install},
+          {path: 'intro', component: md('intro')},
+          {path: 'get-started', component: md('start')},
+          {path: 'install', component: md('install')},
           {path: 'switch', component: SwitchDemo},
           {path: 'button', component: ButtonDemo},
           {path: 'dialog', component: DialogDemo},
